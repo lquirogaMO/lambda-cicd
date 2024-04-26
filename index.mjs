ab26e60419919bae06e3d514ac65a3f22c798be4
Leandro <lquiroga@morrisopazo.com>
2024-04-26 13:33:11 -0300

    commit to lambda update 2

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Lambda VS Code from S3 Notification'),
+      body: JSON.stringify('Hello from Lambda VS Code from S3 Notification to Lambda'),
     };
     return response;
   };
